e0b542e38da9b58b5bbdf1d68067ee2bf7abc701
huang <huangdey973@gmail.com>
2017-11-21 15:17:16 +0100

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+{
+  task: {
+    id:
+    title: "La tour d'argent",
+    detail: "15 Quai de la Tournelle"
+  }
+}
